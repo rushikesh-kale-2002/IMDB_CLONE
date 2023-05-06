9c3983fc6914876bdb07d902b2028e5d62217d5b
rushikesh-kale-2002 <rushikale910@gmail.com>
2023-05-06 10:17:51 +0530

    done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,10 +10,11 @@ const Home = () => {
     const [ popularMovies, setPopularMovies ] = useState([])
 
     useEffect(() => {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=50444869bdac66035d5335e977bee426&language=en-US")
         .then(res => res.json())
         .then(data => setPopularMovies(data.results))
     }, [])
+    
 
     return (
         <>
